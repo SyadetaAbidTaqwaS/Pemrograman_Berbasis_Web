75103f673594267d4d704cfdf14d5c73b27cbcb6
SyadetaAbidTaqwaS <abidtaqwa12@gmail.com>
2026-03-11 19:14:23 +0700

    Tugas Praktikum 5

--- PRAKTIKUM_5/index.php
+++ PRAKTIKUM_5/index.php
@@ -1,16 +1,17 @@
 <?php
-$namabarang = ["Komputer", "Laptop", "Mouse", "Keyboard"];
-$hargasatuan = [900, 700, 300, 150];
+$namabarang = ["Mouse", "Memori", "Laptop", "Keyboard"];
+$hargasatuan = [200, 500, 900, 150];
 $jumlahbeli = 2;
-$pajak = 0.1;
 $totalharga = $jumlahbeli * $hargasatuan[3];
+define("PAJAK", 0.1);
+$pajak = $totalharga * PAJAK ;
 
-echo "<h1>Perhitungan Total Pembelian Barang (Dengan Array) </h1>";
-echo "<hr style='width:40%; border:2px solid black; margin-left:0;'>";
-echo "Nama Barang : " . $namabarang[3] . "<br>";
-echo "Harga Satuan : Rp " . $hargasatuan[3] . ".000" . "<br>";
-echo "Jumlah Beli : " . $jumlahbeli . "<br>";
-echo "Total Harga (Sebelum Pajak) : Rp " . $totalharga . ".000" . "<br>";
-echo "Pajak (10%): Rp " . $pajak * $totalharga . ".000" . "<br>";
-echo "<b>Total Bayar : Rp </b>" . $totalharga + $totalharga * $pajak . "<b>.000</b>" . "<br>";
+echo "<h2><b>Perhitungan Total Pembelian Barang (Dengan Array)</b> </h2>";
+echo "<hr>";
+echo "Nama Barang: " . $namabarang[3] . "<br>";
+echo "Harga Satuan: Rp " . $hargasatuan[3] . ".000" . "<br>";
+echo "Jumlah Beli: " . $jumlahbeli . "<br>";
+echo "Total Harga (Sebelum Pajak): Rp " . $totalharga . ".000" . "<br>";
+echo "Pajak (10%): Rp " . $pajak . ".000" . "<br>";
+echo "<b>Total Bayar: Rp </b>" . ($totalharga + $pajak) . "<b>.000</b>" . "<br>";
 ?>
